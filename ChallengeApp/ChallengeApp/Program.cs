43b5cfb51a98b557d9bd6a862d062d81e369f912
WojciechSta <123731293+WojciechSta@users.noreply.github.com>
2023-02-27 13:29:23 +0100

    Zadanie dzień 4

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,20 @@
+﻿string name = "Ewa";
+bool isWoman = false;
+var age = 16;
+
+if (isWoman && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+
+else if (age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+
+
+else if (age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
