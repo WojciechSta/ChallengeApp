7fb7f3d1ea0a798ee4916602cc2374b68f6e6bef
WojciechSta <123731293+WojciechSta@users.noreply.github.com>
2023-02-27 13:30:10 +0100

    Zadanie dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,20 +1,72 @@
-﻿string name = "Ewa";
-bool isWoman = false;
-var age = 16;
+﻿int number = 02059;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
 
-if (isWoman && age < 30)
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+
+
+
+foreach (char letter in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
 }
-
-
-else if (age == 33)
-{
-    Console.WriteLine("Ewa lat 33");
-}
-
-
-else if (age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
+Console.WriteLine($" Mój numer to 02059");
+Console.WriteLine($" 0 jest 2");
+Console.WriteLine($" 1 jest 0");
+Console.WriteLine($" 2 jest 1");
+Console.WriteLine($" 3 jest 0");
+Console.WriteLine($" 4 jest 0");
+Console.WriteLine($" 5 jest 2");
+Console.WriteLine($" 6 jest 0");
+Console.WriteLine($" 7 jest 0");
+Console.WriteLine($" 8 jest 0");
+Console.WriteLine($" 9 jest 1");
