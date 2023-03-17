3b8fe71abad0e1b783d4f35ee4d8abfeea1b54c0
WojciechSta <123731293+WojciechSta@users.noreply.github.com>
2023-03-17 11:38:16 +0100

    Zadanie  9

---
+++ ChallengeApp/EmployeTests/EmployeeTests.cs
@@ -0,0 +1,24 @@
+﻿using ChallengeApp;
+
+namespace ChalllengeApp.Tests
+{
+    public class EmployeeTests
+    {
+        [Test]
+        public void ResoultStatistics_ShouldReturnMinMaxAverage()
+        {
+            //arange
+            var employee = new Employee("Jan", "Tyczka");
+            employee.AddGrade(4);
+            employee.AddGrade(2);
+            employee.AddGrade(6);
+            //act
+            var statistics = employee.GetStatistics();
+            //assert
+            Assert.AreEqual(2, statistics.Min);
+            Assert.AreEqual(6, statistics.Max);
+            Assert.AreEqual(4, statistics.Average);
+            Assert.AreNotEqual(statistics.Min, statistics.Max);
+        }
+    }
+}
